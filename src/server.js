cb42cefe61959daac34a5e445cfc025c6043d0dc
Gianluca Carra <contact@giancarra.com>
2024-06-24 15:06:35 -0400

    CORS url config

--- src/server.js
+++ src/server.js
@@ -13,6 +13,8 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     "https://food-explorer-web-ten.vercel.app",
+    "https://food-explorer-web-gianlucacarras-projects.vercel.app",
+    "https://food-explorer-web-git-main-gianlucacarras-projects.vercel.app"
   ],
   credentials: true
 }));
