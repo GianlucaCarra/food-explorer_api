e0380e1443f69d6b00d3eff0619778645e2f3412
Gianluca Carra <contact@giancarra.com>
2024-06-24 20:12:51 -0400

    Cookie final

--- src/middlewares/ensureAuth.js
+++ src/middlewares/ensureAuth.js
@@ -4,7 +4,6 @@ const AppError = require("../utils/AppError");
 
 function ensureAuth(req, res, next) {
   const cookies = req.cookies;
-  console.log(cookies)
 
   if(!cookies.token) {
     throw new AppError('Not available JWT');
